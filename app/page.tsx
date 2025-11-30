8dd69b158e1900f8dde34dbe81c897ad33a8aca7
SanketPathare <sanketpathare8808@gmail.com>
2025-11-30 17:42:51 +0530

    WhyChooseUs component to Home page and organize imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import ExpertiseSection from "./Components/Home/expertiseItems";
 import HeroSection from "./Components/Home/HeroSection";
+import WhyChooseUs from "./Components/Home/WhyChooseUs";
 
 export default function Home() {
   return (
     <div>
     <HeroSection/>
-    
+    <ExpertiseSection/>
+    <WhyChooseUs/>
     </div>
   );
 }

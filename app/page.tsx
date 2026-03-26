5c99b215ca5512e0834eae001bdb17a0947e0da4
SanketPathare <sanketpathare8808@gmail.com>
2026-03-26 19:44:55 +0530

    add OurWork component to showcase projects on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ExpertiseSection from "./Components/Home/expertiseItems";
 import HeroSection from "./Components/Home/HeroSection";
+import OurWork from "./Components/Home/OurWork";
 import WhyChooseUs from "./Components/Home/WhyChooseUs";
 
 export default function Home() {
@@ -8,6 +9,8 @@ export default function Home() {
     <HeroSection/>
     <ExpertiseSection/>
     <WhyChooseUs/>
+    <OurWork/>
+
     </div>
   );
 }

4ad1c94eba0c9c7fb116e1d0115f405b536872da
sanket <sanketpathare8808@gmail.com>
2026-05-16 19:47:10 +0530

    feat: implement home page structure with section components

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from "./Components/Home/HeroSection";
 import WhyChooseUs from "./Components/Home/WhyChooseUs";
 import ProcessSection from "./Components/Home/ProcessSection";
 import EngagementModels from "./Components/Home/EngagementModels";
-import ExpertiseSection from "./Components/Home/ExpertiseItems";
+import ExpertiseSection from "./Components/Home/ExpertiseSection";
 import OurServices from "./Components/Home/OurServices";
 
 export default function Home() {
